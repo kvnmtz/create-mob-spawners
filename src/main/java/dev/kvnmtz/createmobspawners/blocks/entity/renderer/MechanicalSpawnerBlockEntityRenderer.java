a79bce46eb3094f337fdbaa0139e9ff88b62f594
kvnmtz <70778888+kvnmtz@users.noreply.github.com>
2025-01-17 18:50:38 +0100

    refactor: extract block entity rendering steps

--- src/main/java/dev/kvnmtz/createmobspawners/blocks/entity/renderer/MechanicalSpawnerBlockEntityRenderer.java
+++ src/main/java/dev/kvnmtz/createmobspawners/blocks/entity/renderer/MechanicalSpawnerBlockEntityRenderer.java
@@ -36,46 +36,54 @@ public class MechanicalSpawnerBlockEntityRenderer extends KineticBlockEntityRend
         var vb = buffer.getBuffer(RenderType.solid());
         KineticBlockEntityRenderer.renderRotatingKineticBlock(be, getRenderedBlockState(be), ms, vb, light);
 
-        if (be.hasStoredEntity()) {
-            var itemRenderer = Minecraft.getInstance().getItemRenderer();
-            var itemStack = ModItems.SOUL_CATCHER.get().getDefaultInstance();
-            ms.pushPose();
-            ms.translate(0.5f, 0.5f, 0.5f);
-            ms.scale(0.8f, 0.8f, 0.8f);
-            ms.mulPose(Axis.YP.rotationDegrees(getAngleForTe(be, be.getBlockPos(), Direction.Axis.Y) * 180 / (float) Math.PI));
-            itemRenderer.renderStatic(itemStack, ItemDisplayContext.FIXED, light, OverlayTexture.NO_OVERLAY, ms, buffer, be.getLevel(), 1);
-            ms.popPose();
-        }
+        renderSoulCatcher(be, ms, buffer, light);
 
+        renderContainedFluid(be, partialTicks, ms, buffer, light);
+    }
+
+    private void renderSoulCatcher(MechanicalSpawnerBlockEntity be, PoseStack ms, MultiBufferSource buffer, int light) {
+        if (!be.hasStoredEntity()) return;
+
+        var itemRenderer = Minecraft.getInstance().getItemRenderer();
+        var itemStack = ModItems.SOUL_CATCHER.get().getDefaultInstance();
+        ms.pushPose();
+        ms.translate(0.5f, 0.5f, 0.5f);
+        ms.scale(0.8f, 0.8f, 0.8f);
+        ms.mulPose(Axis.YP.rotationDegrees(getAngleForTe(be, be.getBlockPos(), Direction.Axis.Y) * 180 / (float) Math.PI));
+        itemRenderer.renderStatic(itemStack, ItemDisplayContext.FIXED, light, OverlayTexture.NO_OVERLAY, ms, buffer, be.getLevel(), 1);
+        ms.popPose();
+    }
+
+    private void renderContainedFluid(MechanicalSpawnerBlockEntity be, float partialTicks, PoseStack ms, MultiBufferSource buffer, int light) {
         var fluidLevel = be.getTank().getPrimaryTank().getFluidLevel();
-        if (fluidLevel != null) {
-            var capHeight = 1.f/16.f + 1.f/128.f;
-            var tankHullWidth = 1.75f/16.f;
-            var minPuddleHeight = 1.f/16.f;
-            var height = 1.f;
-            var width = 1.f;
-            var totalHeight = height - 2 * capHeight - minPuddleHeight;
-            var level = fluidLevel.getValue(partialTicks);
-            if (!(level < 1.0F / (512 * totalHeight))) {
-                var clampedLevel = Mth.clamp(level * totalHeight, 0.0F, totalHeight);
-                var fluidStack = be.getFluidStack();
-                if (!fluidStack.isEmpty()) {
-                    var top = fluidStack.getFluid().getFluidType().isLighterThanAir();
-                    var xMax = tankHullWidth + width - 2 * tankHullWidth;
-                    var yMin = totalHeight + capHeight + minPuddleHeight - clampedLevel;
-                    var yMax = yMin + clampedLevel;
-                    if (top) {
-                        yMin += totalHeight - clampedLevel;
-                        yMax += totalHeight - clampedLevel;
-                    }
+        if (fluidLevel == null) return;
 
-                    var zMax = tankHullWidth + width - 2 * tankHullWidth;
-                    ms.pushPose();
-                    ms.translate(0.0F, clampedLevel - totalHeight, 0.0F);
-                    FluidRenderer.renderFluidBox(fluidStack, tankHullWidth, yMin, tankHullWidth, xMax, yMax, zMax, buffer, ms, light, false);
-                    ms.popPose();
-                }
+        var capHeight = 1.f / 16.f + 1.f / 128.f;
+        var tankHullWidth = 1.75f / 16.f;
+        var minPuddleHeight = 1.f / 16.f;
+        var height = 1.f;
+        var width = 1.f;
+        var totalHeight = height - 2 * capHeight - minPuddleHeight;
+        var level = fluidLevel.getValue(partialTicks);
+        if (level < 1.0F / (512 * totalHeight)) return;
+
+        var clampedLevel = Mth.clamp(level * totalHeight, 0.0F, totalHeight);
+        var fluidStack = be.getFluidStack();
+        if (!fluidStack.isEmpty()) {
+            var top = fluidStack.getFluid().getFluidType().isLighterThanAir();
+            var xMax = tankHullWidth + width - 2 * tankHullWidth;
+            var yMin = totalHeight + capHeight + minPuddleHeight - clampedLevel;
+            var yMax = yMin + clampedLevel;
+            if (top) {
+                yMin += totalHeight - clampedLevel;
+                yMax += totalHeight - clampedLevel;
             }
+
+            var zMax = tankHullWidth + width - 2 * tankHullWidth;
+            ms.pushPose();
+            ms.translate(0.0F, clampedLevel - totalHeight, 0.0F);
+            FluidRenderer.renderFluidBox(fluidStack, tankHullWidth, yMin, tankHullWidth, xMax, yMax, zMax, buffer, ms, light, false);
+            ms.popPose();
         }
     }
 }
